078f4b68c8cee086486f95a0a6074ae68b4cdf43
Tiffany Conroy <tiffany.conroy@gmail.com>
2011-01-28 00:39:26 +0100

    replaced §.getJSON with google.maps.Geocoder()

--- script.js
+++ script.js
@@ -4,50 +4,39 @@
   // http://code.google.com/apis/maps/documentation/javascript/
 
   var showMap = function (options) {
-    var useFullAPI = options.useFullAPI || false; // allows direct access to full V3 api
-
     // Can pass either an object, or just the location as a string
     var location = options.location || options || alert('No location provided for map');
-    var zoom = options.zoom || 13;
+    var zoom = options.zoom || 14;
     var disableDefaultUI = (options.disableDefaultUI === false ? false : true);
-    var mapTypeId = options.mapTypeId || 'HYBRID'; // other: SATELLITE, ROADMAP, TERRAIN
-        mapTypeId = google.maps.MapTypeId[mapTypeId];
+    var mapType = options.mapType || 'HYBRID'; // other: SATELLITE, ROADMAP, TERRAIN
+    var mapTypeId = google.maps.MapTypeId[mapType];
 
-    var renderMap = function (lat, lng) {
-      var map;
-      var latlng = new google.maps.LatLng(lat,lng);
-      var mapOptions = (function(){
-        if (useFullAPI) {
-          return options.mapOptions;
-        } else {
-          return {
+    var renderMap = function (latlng) {
+      var mapOptions = {
             zoom : zoom,
             center : latlng,
             disableDefaultUI : disableDefaultUI,
             mapTypeId : mapTypeId
           };
-        }
-      }());
-      // defining this variable renders the map to "map_canvas"
+      // renders the map to "map_canvas"
       var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
     };
     
-    if (useFullAPI) {
-        renderMap();
-    } else {
-      $.getJSON("http://maps.google.com/maps/geo?q="+location+"&sensor=false&output=json&callback=?",
-        function(data, textStatus){
-          var coords = data.Placemark[0].Point.coordinates;
-          console.log(coords);
-          renderMap(coords[1],coords[0]);
-        });
-    }
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode( { address: location }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        renderMap(results[0].geometry.location);
+      } else {
+        alert("Could not locate " + location);
+      }
+    });
+
     var uri = 'http://maps.google.com/maps?q=' + location;
     document.getElementById('map_link').href = uri;
   };
   
   var options = {
-    location: 'Whistler,%20BC',
+    location: 'Whistler, BC',
     zoom: 10
   };
   showMap(options);
